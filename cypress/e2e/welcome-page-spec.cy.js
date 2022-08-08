941abfffcb04ac6ff1e973939a440f6e46c0330a
Nick Orlov <ialwaysreply@gmail.com>
2022-08-08 01:07:55 -0700

    Make intercept and set up the test environment

--- cypress/e2e/welcome-page-spec.cy.js
+++ cypress/e2e/welcome-page-spec.cy.js
@@ -1,12 +1,12 @@
-import cypress from 'cypress'
-
 describe('Welcome Page', () => {
   beforeEach (() => {
+    cy.intercept('GET',  `https://gutendex.com/books/?copyright=false&languages=en&page=1`, {fixture: 'bookList'})
     cy.visit('http://localhost:3000/')
   })
 
+
 it('Should have a title', () => {
-  cy.visit()
+  cy.get('h1').contains('Read Now')
 })
 
 
