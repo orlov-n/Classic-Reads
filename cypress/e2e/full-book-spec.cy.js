112f7cf75ee75d9979d9255a8cea0265a964635e
Nick Orlov <ialwaysreply@gmail.com>
2022-08-08 03:00:22 -0700

    Add tests

---
+++ cypress/e2e/full-book-spec.cy.js
@@ -0,0 +1,15 @@
+describe('Full Book', () => {
+  beforeEach (() => {
+    cy.intercept('GET', `https://gutendex.com/books/36752`, {fixture: 'book'})
+    cy.visit('http://localhost:3000/book/36752')
+  })
+
+  it('Should display book', () => {
+    cy.get('.full-book').should('have.attr', 'src', "https://www.gutenberg.org/ebooks/36752.html.images" )
+  })
+
+  it.skip('Should be able to change pages with scroll bar', () => {
+    cy.scrollTo('bottom').window('.full-book').its('scrollY').should('not.equal', 0)
+  })
+
+})
