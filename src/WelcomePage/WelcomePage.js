27f39b4a487efa8653c61c061836e3dc1a07e9dd
Nick Orlov <ialwaysreply@gmail.com>
2022-08-21 05:59:26 -0700

    Add random button

--- src/WelcomePage/WelcomePage.js
+++ src/WelcomePage/WelcomePage.js
@@ -1,32 +1,34 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./WelcomePage.css";
 import BookCard from "../BookCard/BookCard";
 import { NavLink } from "react-router-dom";
 
-
-export const WelcomePage = ({  book, randomBookList, pageId, goToNextPage, refreshBooklist }) => {
-    const [bookLink, setBookLink] = useState("");
-    // const [randomBookList, setRandomBookList] = useState([]);
-    console.log('book from welcome page', book)
-    useEffect(() => {
-      // getBook(bookId).then((response) => {
-      //   setBookLink(response.formats["text/html"]);
-      // });
-    }, [ book ]);
-    
-
-        
-      
-    
+export const WelcomePage = ({
+  book,
+  randomBookList,
+  pageId,
+  goToNextPage,
+  refreshBooklist,
+}) => {
+  const [bookLink, setBookLink] = useState("");
+  // const [randomBookList, setRandomBookList] = useState([]);
+  console.log("book from welcome page", book);
+  useEffect(() => {
+    // getBook(bookId).then((response) => {
+    //   setBookLink(response.formats["text/html"]);
+    // });
+  }, [book]);
 
   return (
     <section className="welcome-page-container">
       <h2>WELCOME</h2>
+      <button onClick={() => window.location.reload()}>
+        Generate New Random Book
+      </button>
 
       <NavLink to={`/full-book/${book.id}`} key={book.id}>
-            <BookCard bookCardProp={book} />
-          </NavLink>
-
+        <BookCard bookCardProp={book} />
+      </NavLink>
     </section>
   );
 };
