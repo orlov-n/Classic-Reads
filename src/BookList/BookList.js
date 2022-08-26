1f6facbbcbccccfa6fb14d0c3373566338dc9c15
Nick Orlov <ialwaysreply@gmail.com>
2022-08-25 20:04:39 -0700

    Secure next button in place

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -25,9 +25,11 @@ const BookList = ({ bookListProp, pageId }) => {
 
   return (
     <section className="book-list-container">
+      <div className="next-previous-container">
       <NavLink to={`/page/${pageId + 1}`} style={{ textDecoration: "none" }}>
-        <button>Next Page</button>
+        <button className="next-page-button">Next Page</button>
       </NavLink>
+      </div>
       <div className="book-cards-grid">
 
       {bookListProp && renderBookCards()}
